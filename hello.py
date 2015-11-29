2a0eab64d898891a977eb9fe8366e640330e16d8
hb6h057 <me@hb6.org>
2015-11-30 00:02:52 +0800

    add templates && static

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
 #!env/bin/python
 
+import os
+import random
+
 import tornado.httpserver
 import tornado.ioloop
 import tornado.options
@@ -12,12 +15,33 @@ define("port", default="8080", help="run on the given port", type=int)
 
 class IndexHandler(tornado.web.RequestHandler):
     def get(self):
-        greeting = self.get_argument('greeting', 'Guest')
-        self.write(greeting + 'say: "Hello World!"')
+        self.render('default.html')
+
+class ResultHandler(tornado.web.RequestHandler):
+    def firend(self, name):
+        return 'my dear firend %s' % (name)
+
+    def post(self):
+        greeting = self.get_argument('name', 'Guest')
+        self.render('result.html', greeting=greeting, firend=self.firend)
+
 
 if __name__ == '__main__':
+
+    handler_list = [
+        (r'/', IndexHandler),
+        (r'/result', ResultHandler),
+    ]
+
     tornado.options.parse_command_line()
-    app = tornado.web.Application(handlers=[(r"/", IndexHandler)], debug=True)
+
+    app = tornado.web.Application(
+        handlers=handler_list,
+        template_path=os.path.join(os.path.dirname(__file__), "templates"),
+        static_path=os.path.join(os.path.dirname(__file__), "static"),
+        debug=True,
+    )
+
     http_server = tornado.httpserver.HTTPServer(app)
     http_server.listen(8080)
     tornado.ioloop.IOLoop.instance().start()
