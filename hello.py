867c673524b40129170f2c27cef4fac639ad4922
hb6h057 <me@hb6.org>
2016-03-06 03:52:47 +0800

    learning tornado document

--- hello.py
+++ hello.py
@@ -12,34 +12,58 @@ from tornado.options import define, options
 
 define("port", default="8080", help="run on the given port", type=int)
 
+SETTING = dict(
+    template_path=os.path.join(os.path.dirname(__file__), "templates"),
+    static_path=os.path.join(os.path.dirname(__file__), "static"),
+    debug=True,
+    cookie_secret='buyayongroot'
+)
+
+class BaseHandler(tornado.web.RequestHandler):
+    def get_current_user(self):
+        return self.get_secure_cookie("user")
 
 class IndexHandler(tornado.web.RequestHandler):
     def get(self):
-        self.render('default.html')
+        name = None
+        if not self.current_user:
+            name = self.get_secure_cookie('user')
 
-class ResultHandler(tornado.web.RequestHandler):
+        self.render('default.html', name=name)
+
+class ResultHandler(BaseHandler):
     def firend(self, name):
         return 'my dear firend %s' % (name)
 
     def post(self):
         greeting = self.get_argument('name', 'Guest')
-        self.render('result.html', greeting=greeting, firend=self.firend)
+
+        if greeting == 'hacker':
+            self.redirect('/test')
+        elif greeting != 'Guest' and not self.current_user:
+            self.set_secure_cookie('user', greeting)
+
+        self.render('result.html', greeting=greeting, firend=self.firend,
+                    path=self.request.path, headers=self.request.headers)
+
+class HTTPErrorHandler(tornado.web.RequestHandler):
+    def get(self):
+        raise tornado.web.HTTPError(403)
 
 
 if __name__ == '__main__':
 
-    handler_list = [
-        (r'/', IndexHandler),
-        (r'/result', ResultHandler),
-    ]
-
     tornado.options.parse_command_line()
 
     app = tornado.web.Application(
-        handlers=handler_list,
-        template_path=os.path.join(os.path.dirname(__file__), "templates"),
-        static_path=os.path.join(os.path.dirname(__file__), "static"),
-        debug=True,
+        handlers = [
+            (r'/', IndexHandler),
+            (r'/result', ResultHandler),
+            (r'/test', HTTPErrorHandler),
+            (r'/(static_file\.txt)', tornado.web.StaticFileHandler,
+                                     dict(path=SETTING['static_path'])),
+        ],
+        **SETTING
     )
 
     http_server = tornado.httpserver.HTTPServer(app)
